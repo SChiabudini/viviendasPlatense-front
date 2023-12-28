98353cba27a2e44ce12ca4b188870c51b17a805c
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2023-12-28 15:50:10 -0300

    banner 2 DONE

--- src/components/views/home/banner/Banner.jsx
+++ src/components/views/home/banner/Banner.jsx
@@ -20,8 +20,19 @@ const Banner = () => {
                     </div>
 
                     <div className={style.banner2}>
-                        <img src="" alt=""/>
-                        <p>Div 2</p>
+                        <div className={style.banner2bg}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703695382/viviendasPlatense/resources/banners/2/be2uo8rfazjpp0ym0sih.svg" alt=""/></div>
+                        <div className={style.content}>
+                            <div className={style.contentImg}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703695383/viviendasPlatense/resources/banners/2/kv8l0ykuah3logoerzqb.png" alt="Equipos de Viviendas Platense y Rescataditos La Plata"/></div>
+                            <div className={style.contentText}>
+                                <p className={style.textBlue}>Con la compra de tu casa<br/>colaborás con <a href="https://www.instagram.com/rescataditoslaplata/" target="_blank" rel="noopener noreferrer">Rescataditos La Plata</a></p>
+                                <div className={style.textBottom}>
+                                    <p><span className={style.textRed}>Adoptá, no compres</span><br/>Tu hogar es mejor con una mascota</p>
+                                    <p className={style.contentLogo}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703785028/viviendasPlatense/resources/banners/2/nthm8p4xal6a9nbqjzdu.png" alt="Rescataditos La Plata"/></p>
+                                    <p className={style.contentLogo}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703785028/viviendasPlatense/resources/banners/2/xhdsjdxjdtzdzb6iflth.png" alt="Viviendas Platense"/></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={style.vector}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703695384/viviendasPlatense/resources/banners/2/hrc5kn33yg8jnfvnlhr6.svg" alt=""/></div>
                     </div>
 
                     <div className={style.banner3}>
