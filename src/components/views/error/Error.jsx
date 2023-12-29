ace8bfa63871d806b770f88bae335552fd78baee
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2023-12-29 14:10:25 -0300

    error page DONE

--- src/components/views/error/Error.jsx
+++ src/components/views/error/Error.jsx
@@ -1,7 +1,13 @@
+import { NavLink } from 'react-router-dom';
+import style from './Error.module.css';
+
 const Error = () => {
     return(
-        <div>
-            <p>Soy el error</p>
+        <div className={style.div}>
+            <p>ERROR 404</p>
+            <p className={style.second}>Página no encontrada</p>
+            <div className={style.image}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703097726/viviendasPlatense/resources/runhzi5qcoiicsdtrfdv.svg" alt="Viviendas Platense"/></div>
+            <NavLink to="/">Volver</NavLink>
         </div>
     );
 }
