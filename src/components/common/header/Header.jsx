d07568378d688b258665733ddc1e8de40e197e74
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2024-01-03 16:00:46 -0300

    hotfix

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,12 +1,13 @@
 import Nav from './nav/Nav';
 import logoHeader from './logo-nav.svg';
 import style from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return(
         <div className={style.div}>
             <div className={style.content}>
-                <h1><img src={logoHeader} alt="Viviendas Platense" title="Viviendas Platense - Construcciones Industrializadas"/></h1>
+                <NavLink to="/"><h1><img src={logoHeader} alt="Viviendas Platense" title="Viviendas Platense - Construcciones Industrializadas"/></h1></NavLink>
                 <div className={style.nav}><Nav/></div>
             </div>
         </div>
