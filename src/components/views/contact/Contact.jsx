3ec9283453dc5810aeaa72b369f6e20d4acbfc47
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2024-01-02 22:13:35 -0300

    catalogue responsiveness DONE

--- src/components/views/contact/Contact.jsx
+++ src/components/views/contact/Contact.jsx
@@ -3,12 +3,22 @@ import Form from './form/Form';
 import style from './Contact.module.css';
 
 const Contact = () => {
-    return(
+    return (
         <div className={style.div}>
             <h2 className={style.heading}>VIVIENDAS PLATENSE</h2>
-            <div className={style.content}>            
-                <div className={`${style.article} ${style.info}`}><Info/></div>
-                <div className={`${style.article} ${style.form}`}><Form/></div>
+            <div className={style.content}>
+                {/* Cambio de orden en pantallas menores a 721px */}
+                {window.innerWidth <= 721 ? (
+                    <>
+                        <div className={`${style.article} ${style.form}`}><Form /></div>
+                        <div className={`${style.article} ${style.info}`}><Info /></div>
+                    </>
+                ) : (
+                    <>
+                        <div className={`${style.article} ${style.info}`}><Info /></div>
+                        <div className={`${style.article} ${style.form}`}><Form /></div>
+                    </>
+                )}
             </div>
         </div>
     );
