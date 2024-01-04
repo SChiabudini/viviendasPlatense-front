80ce0cd139750b04b369995b63f93d80e47e309c
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2024-01-04 19:02:30 -0300

    mobile menu test

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,15 +1,48 @@
+import React, { useState } from 'react';
 import Nav from './nav/Nav';
 import logoHeader from './logo-nav.svg';
 import style from './Header.module.css';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-    return(
-        <div className={style.div}>
-            <div className={style.content}>
-                <NavLink to="/"><h1><img src={logoHeader} alt="Viviendas Platense" title="Viviendas Platense - Construcciones Industrializadas"/></h1></NavLink>
-                <div className={style.nav}><Nav/></div>
-            </div>
+    const [menuOpen, setMenuOpen] = useState(true);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    let isMobile = window.innerWidth <= 720;
+
+    return (
+        <div>
+            {isMobile ? (
+                <div className={style.div}>
+                    <div className={style.content}>
+                        <NavLink to="/">
+                            <h1><img src={logoHeader} alt="Viviendas Platense" title="Viviendas Platense - Construcciones Industrializadas" /></h1>
+                        </NavLink>
+                        <div className={style.mobileMenu} onClick={toggleMenu}>
+                            <div className={style.bar}></div>
+                            <div className={style.bar}></div>
+                            <div className={style.bar}></div>
+                        </div>
+                    </div>
+                    {menuOpen ? (
+                        <div className={style.nav}><Nav /></div>
+                    ) : (
+                        <div></div>
+                    )}
+                </div>
+            ) : (
+                <div className={style.div}>
+                    <div className={style.content}>
+                        <NavLink to="/">
+                            <h1><img src={logoHeader} alt="Viviendas Platense" title="Viviendas Platense - Construcciones Industrializadas" /></h1>
+                        </NavLink>
+                        <div className={style.nav}><Nav /></div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
