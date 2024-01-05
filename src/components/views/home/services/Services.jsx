27ec7b5d64593d2fad84957c5df585d575e1aac5
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2024-01-05 19:55:12 -0300

    services mobile css done

--- src/components/views/home/services/Services.jsx
+++ src/components/views/home/services/Services.jsx
@@ -1,9 +1,28 @@
 import style from './Services.module.css';
+import { useState, useEffect } from 'react';
 
 const Services = () => {
+
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 720);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 720);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return(
         <div className={style.div}>
-            <div className={style.bgImg}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703097578/viviendasPlatense/resources/ggezzfw4fnmswbgcxpxb.jpg" alt=""/></div>
+            {isMobile ? (
+                <div className={style.bgImg}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1704494370/viviendasPlatense/resources/jl7v8gjahudyfqfw0mli.jpg" alt=""/></div>
+            ) : (
+                <div className={style.bgImg}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703097578/viviendasPlatense/resources/ggezzfw4fnmswbgcxpxb.jpg" alt=""/></div>
+            )}
+        
             <div className={style.content}>
                 <div className={style.image}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703097726/viviendasPlatense/resources/runhzi5qcoiicsdtrfdv.svg" alt="Viviendas Platense"/></div>
                 <div className={style.info}>
