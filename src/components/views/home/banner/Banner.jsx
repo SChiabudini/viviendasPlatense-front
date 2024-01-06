694caa87d3ec0f3f57de15bbf4f08f4bc3bbd912
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2024-01-06 03:30:29 -0300

    banners mobile css DONE

--- src/components/views/home/banner/Banner.jsx
+++ src/components/views/home/banner/Banner.jsx
@@ -1,8 +1,22 @@
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { useState, useEffect } from 'react';
 import style from "./Banner.module.css";
 
 const Banner = () => {
+
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 720);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 720);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return(
         <div className={style.banner}>
             <Carousel showThumbs={false} showStatus={false} infiniteLoop={true} autoPlay={false} interval={5000} transitionTime={1000}>
@@ -22,17 +36,36 @@ const Banner = () => {
 
                     <div className={style.banner2}>
                         <div className={style.banner2bg}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703695382/viviendasPlatense/resources/banners/2/be2uo8rfazjpp0ym0sih.svg" alt=""/></div>
-                        <div className={style.content}>
-                            <div className={style.contentImg}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703695383/viviendasPlatense/resources/banners/2/kv8l0ykuah3logoerzqb.png" alt="Equipos de Viviendas Platense y Rescataditos La Plata"/></div>
-                            <div className={style.contentText}>
-                                <p className={style.textBlue}>Con la compra de tu casa<br/>colaborás con <a href="https://www.instagram.com/rescataditoslaplata/" target="_blank" rel="noopener noreferrer">Rescataditos La Plata</a></p>
-                                <div className={style.textBottom}>
-                                    <p><span className={style.textRed}>Adoptá, no compres</span><br/>Tu hogar es mejor con una mascota</p>
-                                    <p className={style.contentLogo}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703785028/viviendasPlatense/resources/banners/2/nthm8p4xal6a9nbqjzdu.png" alt="Rescataditos La Plata"/></p>
-                                    <p className={style.contentLogo}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703785028/viviendasPlatense/resources/banners/2/xhdsjdxjdtzdzb6iflth.png" alt="Viviendas Platense"/></p>
+                        
+                        {isMobile ? (
+                            <div className={style.content}>
+                                <div className={style.contentText}>
+                                    <p className={style.textBlue}>Con la compra de tu casa<br/>colaborás con <a href="https://www.instagram.com/rescataditoslaplata/" target="_blank" rel="noopener noreferrer">Rescataditos La Plata</a></p>
+                                    <div className={style.textBottom}>
+                                        <p><span className={style.textRed}>Adoptá, no compres</span><br/>Tu hogar es mejor con una mascota</p>
+                                        <p className={style.contentLogo}>
+                                            <img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703785028/viviendasPlatense/resources/banners/2/nthm8p4xal6a9nbqjzdu.png" alt="Rescataditos La Plata"/>
+                                            <img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703785028/viviendasPlatense/resources/banners/2/xhdsjdxjdtzdzb6iflth.png" alt="Viviendas Platense"/>
+                                        </p>
+                                        
+                                    </div>
+                                </div>
+                                <div className={style.contentImg}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703695383/viviendasPlatense/resources/banners/2/kv8l0ykuah3logoerzqb.png" alt="Equipos de Viviendas Platense y Rescataditos La Plata"/></div>
+                            </div>
+                        ) : (
+                            <div className={style.content}>
+                                <div className={style.contentImg}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703695383/viviendasPlatense/resources/banners/2/kv8l0ykuah3logoerzqb.png" alt="Equipos de Viviendas Platense y Rescataditos La Plata"/></div>
+                                <div className={style.contentText}>
+                                    <p className={style.textBlue}>Con la compra de tu casa<br/>colaborás con <a href="https://www.instagram.com/rescataditoslaplata/" target="_blank" rel="noopener noreferrer">Rescataditos La Plata</a></p>
+                                    <div className={style.textBottom}>
+                                        <p><span className={style.textRed}>Adoptá, no compres</span><br/>Tu hogar es mejor con una mascota</p>
+                                        <p className={style.contentLogo}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703785028/viviendasPlatense/resources/banners/2/nthm8p4xal6a9nbqjzdu.png" alt="Rescataditos La Plata"/></p>
+                                        <p className={style.contentLogo}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703785028/viviendasPlatense/resources/banners/2/xhdsjdxjdtzdzb6iflth.png" alt="Viviendas Platense"/></p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        )}
+                        
                         <div className={style.vector}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703695384/viviendasPlatense/resources/banners/2/hrc5kn33yg8jnfvnlhr6.svg" alt=""/></div>
                     </div>
 
