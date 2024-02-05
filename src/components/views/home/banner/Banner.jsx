bd06a9eb958a0dc61dd4f10ebb8107e666ed8cf0
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2024-02-05 20:57:33 -0300

    banners images links fixed

--- src/components/views/home/banner/Banner.jsx
+++ src/components/views/home/banner/Banner.jsx
@@ -32,17 +32,17 @@ const Banner = () => {
                                     <p className={style.text}>Flete y armado bonificados</p>
                                     <p className={style.text}>OFERTA PAGANDO AL CONTADO</p>
                                     <p className={style.number}>
-                                        <img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1705693188/viviendasPlatense/resources/jslnqlqqo76xwukqlhke.svg" alt=""/>
+                                        <img src="https://res.cloudinary.com/dsmrctezd/image/upload/v1707175916/viviendasPlatense/resources/banners/1/pcnmetyslpoywjomfzrk.svg" alt=""/>
                                         <a href='https://wa.me/542216911442?text=Buenos%20d%C3%ADas,%20quisiera%20consultar%20por%20el%20modelo%20en%20promoci%C3%B3n.%20Muchas%20gracias.'>Envianos un WhatsApp </a>
                                         
                                     </p></div>
-                                <div className={style.banner1render}><img  src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703024124/viviendasPlatense/houses/22%2C5%20Americana%20-%201%20Dormitorio/rhktmrz63dvzbfflohws.jpg" alt="22,5 Americana - 1 Dormitorio - En promoción"/></div>
+                                <div className={style.banner1render}><img  src="https://res.cloudinary.com/dsmrctezd/image/upload/v1707175492/viviendasPlatense/houses/22%2C5%20Americana%20-%201%20Dormitorio/klrkfgs3029bkzq69kaz.jpg" alt="22,5 Americana - 1 Dormitorio - En promoción"/></div>
                             </>
                         ) : (
                             <>
                                 <div className={style.banner1bg}><img src="https://res.cloudinary.com/dsmrctezd/image/upload/v1707175912/viviendasPlatense/resources/banners/1/qhz4yugxeko7llruidob.svg" alt=""/></div>
                                 <div className={style.banner1render}><img  src="https://res.cloudinary.com/dsmrctezd/image/upload/v1707175916/viviendasPlatense/resources/banners/1/xcmk0fsbkchbyfkbhbxp.jpg" alt="22,5 Americana - 1 Dormitorio - En promoción"/></div>
-                                <div className={style.banner1logo}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703097726/viviendasPlatense/resources/runhzi5qcoiicsdtrfdv.svg" alt=""/></div>
+                                <div className={style.banner1logo}><img src="https://res.cloudinary.com/dsmrctezd/image/upload/v1707176376/viviendasPlatense/resources/g5nt2fvnza5laaikgp8a.svg" alt=""/></div>
                                 <div className={style.banner1text}>
                                     <h3>TU CASA A SOLO</h3>
                                     <p className={style.price}>$3.990.000</p>
@@ -60,7 +60,7 @@ const Banner = () => {
                     </div>
 
                     <div className={style.banner2}>
-                        <div className={style.banner2bg}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703695382/viviendasPlatense/resources/banners/2/be2uo8rfazjpp0ym0sih.svg" alt=""/></div>
+                        <div className={style.banner2bg}><img src="https://res.cloudinary.com/dsmrctezd/image/upload/v1707176309/viviendasPlatense/resources/banners/2/rzowvcdlyfkok5y46kfq.svg" alt=""/></div>
                         
                         {isMobile ? (
                             <div className={style.content}>
@@ -69,29 +69,29 @@ const Banner = () => {
                                     <div className={style.textBottom}>
                                         <p><span className={style.textRed}>Adoptá, no compres</span><br/>Tu hogar es mejor con una mascota</p>
                                         <p className={style.contentLogo}>
-                                            <img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703785028/viviendasPlatense/resources/banners/2/nthm8p4xal6a9nbqjzdu.png" alt="Rescataditos La Plata"/>
-                                            <img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703785028/viviendasPlatense/resources/banners/2/xhdsjdxjdtzdzb6iflth.png" alt="Viviendas Platense"/>
+                                            <img src="https://res.cloudinary.com/dsmrctezd/image/upload/v1707176351/viviendasPlatense/resources/banners/2/w4cns1i6roh1ooiprus1.png" alt="Rescataditos La Plata"/>
+                                            <img src="https://res.cloudinary.com/dsmrctezd/image/upload/v1707176350/viviendasPlatense/resources/banners/2/zxmez46ppqhpju4ngdqc.png" alt="Viviendas Platense"/>
                                         </p>
                                         
                                     </div>
                                 </div>
-                                <div className={style.contentImg}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703695383/viviendasPlatense/resources/banners/2/kv8l0ykuah3logoerzqb.png" alt="Equipos de Viviendas Platense y Rescataditos La Plata"/></div>
+                                <div className={style.contentImg}><img src="https://res.cloudinary.com/dsmrctezd/image/upload/v1707176313/viviendasPlatense/resources/banners/2/u9uuyrrcv0w9terdylwk.png" alt="Equipos de Viviendas Platense y Rescataditos La Plata"/></div>
                             </div>
                         ) : (
                             <div className={style.content}>
-                                <div className={style.contentImg}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703695383/viviendasPlatense/resources/banners/2/kv8l0ykuah3logoerzqb.png" alt="Equipos de Viviendas Platense y Rescataditos La Plata"/></div>
+                                <div className={style.contentImg}><img src="https://res.cloudinary.com/dsmrctezd/image/upload/v1707176313/viviendasPlatense/resources/banners/2/u9uuyrrcv0w9terdylwk.png" alt="Equipos de Viviendas Platense y Rescataditos La Plata"/></div>
                                 <div className={style.contentText}>
                                     <p className={style.textBlue}>Con la compra de tu casa<br/>colaborás con <a href="https://www.instagram.com/rescataditoslaplata/" target="_blank" rel="noopener noreferrer">Rescataditos La Plata</a></p>
                                     <div className={style.textBottom}>
                                         <p><span className={style.textRed}>Adoptá, no compres</span><br/>Tu hogar es mejor con una mascota</p>
-                                        <p className={style.contentLogo}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703785028/viviendasPlatense/resources/banners/2/nthm8p4xal6a9nbqjzdu.png" alt="Rescataditos La Plata"/></p>
-                                        <p className={style.contentLogo}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703785028/viviendasPlatense/resources/banners/2/xhdsjdxjdtzdzb6iflth.png" alt="Viviendas Platense"/></p>
+                                        <p className={style.contentLogo}><img src="https://res.cloudinary.com/dsmrctezd/image/upload/v1707176351/viviendasPlatense/resources/banners/2/w4cns1i6roh1ooiprus1.png" alt="Rescataditos La Plata"/></p>
+                                        <p className={style.contentLogo}><img src="https://res.cloudinary.com/dsmrctezd/image/upload/v1707176350/viviendasPlatense/resources/banners/2/zxmez46ppqhpju4ngdqc.png" alt="Viviendas Platense"/></p>
                                     </div>
                                 </div>
                             </div>
                         )}
                         
-                        <div className={style.vector}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703695384/viviendasPlatense/resources/banners/2/hrc5kn33yg8jnfvnlhr6.svg" alt=""/></div>
+                        <div className={style.vector}><img src="https://res.cloudinary.com/dsmrctezd/image/upload/v1707176339/viviendasPlatense/resources/banners/2/u6trkzi3rxlvm7agfgsl.svg" alt=""/></div>
                     </div>
 
                     <div className={style.banner3}>
@@ -102,17 +102,17 @@ const Banner = () => {
                                 <div className={style.images}>
                                     <div className={style.imageContainer}>
                                         <div className={style.imageOverlay}></div>
-                                        <img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1705326249/viviendasPlatense/houses/36%20Americana%20-%202%20Dormitorios/llnxhiy8wdvswnhvy7dk.jpg" alt="Modelo Americano"/>
+                                        <img src="https://res.cloudinary.com/dsmrctezd/image/upload/v1707175492/viviendasPlatense/houses/36%20Americana%20-%202%20Dormitorios/r3kt7g9hps5qn7dotmn3.jpg" alt="Modelo Americano"/>
                                         <p>Americana</p>
                                     </div>
                                     <div className={style.imageContainer}>
                                         <div className={style.imageOverlay}></div>
-                                        <img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1705327817/viviendasPlatense/houses/79%2C5%20Minimalista%20-%203%20Dormitorios/mjfc1n6gaj9xtxkse7fu.jpg" alt="Modelo Minimalista"/>
+                                        <img src="https://res.cloudinary.com/dsmrctezd/image/upload/v1707175495/viviendasPlatense/houses/79%2C5%20Minimalista%20-%203%20Dormitorios/ba3pbg0gtxyiixhao2cs.jpg" alt="Modelo Minimalista"/>
                                         <p>Minimalista</p>
                                     </div>
                                     <div className={style.imageContainer}>
                                         <div className={style.imageOverlay}></div>
-                                        <img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1705328130/viviendasPlatense/houses/80%20Americana%20-%203%20Dormitorios/sbmd46nl0dxoxvwnrqyz.jpg" alt="Modelo 2 Plantas"/>
+                                        <img src="https://res.cloudinary.com/dsmrctezd/image/upload/v1707175490/viviendasPlatense/houses/80%20Americana%20-%203%20Dormitorios/zlc8j9o20ka3ffasqlfn.jpg" alt="Modelo 2 Plantas"/>
                                         <p>2 Plantas</p>
                                     </div>
                                 </div>
