ee1e73d64fe89af10da716d8ac9029a5ca5fb03b
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2024-02-05 20:59:50 -0300

    error images link fixed

--- src/components/views/error/Error.jsx
+++ src/components/views/error/Error.jsx
@@ -6,7 +6,7 @@ const Error = () => {
         <div className={style.div}>
             <p>ERROR 404</p>
             <p className={style.second}>Página no encontrada</p>
-            <div className={style.image}><img src="https://res.cloudinary.com/dwemuzjbb/image/upload/v1703097726/viviendasPlatense/resources/runhzi5qcoiicsdtrfdv.svg" alt="Viviendas Platense"/></div>
+            <div className={style.image}><img src="https://res.cloudinary.com/dsmrctezd/image/upload/v1707176376/viviendasPlatense/resources/g5nt2fvnza5laaikgp8a.svg" alt="Viviendas Platense"/></div>
             <NavLink to="/">Volver</NavLink>
         </div>
     );
