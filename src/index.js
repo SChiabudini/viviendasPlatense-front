96120a971b9d747251108a5b472c25b0342fe42d
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2024-05-02 13:32:21 -0300

    updated axios url

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from "./redux/store.js";
 
 const apiUrl = process.env.REACT_APP_API_BASE_URL;
 
-axios.defaults.baseURL = apiUrl;
+axios.defaults.baseURL = 'https://viviendasplatenseback.onrender.com';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
