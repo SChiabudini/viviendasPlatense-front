822a5d1622151d78242f21f77676c11a61d9766c
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2025-03-15 19:35:01 -0300

    updated banner

--- src/components/views/home/banner/Banner.jsx
+++ src/components/views/home/banner/Banner.jsx
@@ -28,7 +28,7 @@ const Banner = () => {
                                 <div className={style.banner1text}>                                    
                                     <h3>PROMO ESPECIAL</h3>
                                     <p className={style.price}>BASE DE HORMIGÓN BONIFICADA</p>
-                                    <p className={style.text}><span className={style.textPrice}>$5.692.000</span></p>
+                                    <p className={style.text}><span className={style.textPrice}>$5.760.000</span></p>
                                     <p className={style.text}>Flete y armado bonificados</p>
                                     <p className={style.text}>OFERTA PAGANDO AL CONTADO</p>
                                     <p className={style.number}>
@@ -46,7 +46,7 @@ const Banner = () => {
                                 <div className={style.banner1text}>
                                     <h3>PROMO ESPECIAL</h3>
                                     <p className={style.price}>BASE DE HORMIGÓN BONIFICADA</p>
-                                    <p className={style.text}><span className={style.textPrice}>$5.692.000</span></p>
+                                    <p className={style.text}><span className={style.textPrice}>$5.760.000</span></p>
                                     <p className={style.text}>Flete y armado bonificados</p>
                                     <p className={style.text}>OFERTA PAGANDO AL CONTADO</p>
                                     <p className={style.number}>
